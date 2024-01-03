18ad5288381e2c8ca389fd6b22eb37e61b1cfe6e
Trần Minh Quang <tmquang0209@gmail.com>
2024-01-03 10:05:44 +0700

    update datepicker

--- src/components/datePicker.js
+++ src/components/datePicker.js
@@ -3,20 +3,20 @@ import DateTimePicker from "@react-native-community/datetimepicker";
 import { View, Platform, Text, TouchableOpacity } from "react-native";
 import { black, white } from "../constants/colors";
 
-const DatePickerAndroid = ({ date, mode, onChange }) => {
+const DatePickerAndroid = ({ date, mode, onChange, minimumDate, maximumDate }) => {
     return (
         <View>
-            <DateTimePicker maximumDate={new Date(2010, 11, 31)} value={date} mode={mode} onChange={onChange} />
+            <DateTimePicker minimumDate={minimumDate} maximumDate={maximumDate} value={date} mode={mode} onChange={onChange} />
         </View>
     );
 };
 
-const DatePickerIOS = ({ date, mode, onChange, onClose }) => {
+const DatePickerIOS = ({ date, mode, onChange, onClose, minimumDate, maximumDate }) => {
     return (
         <View
             style={{
                 position: "absolute",
-                backgroundColor: black,
+                backgroundColor: white,
                 width: "100%",
                 zIndex: 1,
                 bottom: 0,
@@ -24,17 +24,25 @@ const DatePickerIOS = ({ date, mode, onChange, onClose }) => {
             }}
         >
             <TouchableOpacity style={{ alignItems: "flex-end", marginRight: 10, marginTop: 10 }} onPress={onClose}>
-                <Text style={{ color: white, fontSize: 20 }}>Close</Text>
+                <Text style={{ color: black, fontSize: 20 }}>Close</Text>
             </TouchableOpacity>
-            <DateTimePicker maximumDate={new Date(2010, 11, 31)} value={date} mode={mode} onChange={onChange} display="spinner" timeZoneName={"Asia/Ho_Chi_minh"} />
+            <DateTimePicker minimumDate={minimumDate} maximumDate={maximumDate} value={date} mode={mode} onChange={onChange} display="spinner" timeZoneName={"Asia/Ho_Chi_minh"} />
         </View>
     );
 };
 
-const DatePicker = ({ value, mode, onChange, onShow }) => {
+const DatePicker = ({ value, mode, onChange, onShow, minimumDate, maximumDate }) => {
     const [date, setDate] = useState(value || new Date(1970, 1, 1));
 
-    return Platform.OS === "ios" ? <DatePickerIOS date={date} mode={mode} onChange={onChange} onClose={onShow} /> : <DatePickerAndroid date={date} mode={mode} onChange={onChange} />;
+    const defaultMinDate = minimumDate ? minimumDate : new Date(1900, 1, 1);
+    1;
+    const defaultMaxDate = maximumDate ? maximumDate : new Date();
+
+    return Platform.OS === "ios" ? (
+        <DatePickerIOS date={date} mode={mode} onChange={onChange} onClose={onShow} minimumDate={defaultMinDate} maximumDate={defaultMaxDate} />
+    ) : (
+        <DatePickerAndroid date={date} mode={mode} onChange={onChange} minimumDate={defaultMinDate} maximumDate={defaultMaxDate} />
+    );
 };
 
 export default DatePicker;
