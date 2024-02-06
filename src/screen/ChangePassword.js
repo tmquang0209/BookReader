28efb9eb246ca06f04a4c89faa4d7c78db4af4c7
Trần Minh Quang <tmquang0209@gmail.com>
2024-02-06 16:31:59 +0700

    validate change password form

--- src/screen/ChangePassword.js
+++ src/screen/ChangePassword.js
@@ -1,36 +1,41 @@
 import React, { useEffect, useState } from "react";
-
 import { Alert, Image, Keyboard, SafeAreaView, ScrollView, Text, TouchableWithoutFeedback, View } from "react-native";
-import styles from "../components/common/styles";
-
-import { gray5, white } from "../constants/colors";
-
 import { connect } from "react-redux";
 import { TextInput } from "react-native-paper";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+import styles from "../components/common/styles";
+
+import { updatePassword } from "../API/authUser";
+import { gray5, white } from "../constants/colors";
+
 import { BackButton, Title } from "../components/header";
 import { Button } from "../components/button";
-import { updatePassword } from "../API/authUser";
+
+const validationSchema = Yup.object().shape({
+    curPassword: Yup.string().required("Current password is required"),
+    newPassword: Yup.string()
+        .required("Password is required")
+        .min(8, "Password must be at least 8 characters")
+        .matches(/[a-zA-Z]/, "Password must contain at least one letter")
+        .matches(/[0-9]/, "Password must contain at least one number")
+        .matches(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character"),
+    rePassword: Yup.string()
+        .required("Re-password is required")
+        .oneOf([Yup.ref("newPassword"), null], "Passwords must match"),
+});
 
 const ChangePassword = (props) => {
     const { user } = props;
 
-    const [info, setInfo] = useState({
-        curPassword: "",
-        newPassword: "",
-        rePassword: "",
-    });
-
     const [disable, setDisable] = useState(false);
     const [show, setShow] = useState(true);
 
-    const handleFieldChange = (name, value) => {
-        setInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
-    };
-
-    const onSubmitPress = async () => {
+    const onSubmitPress = async (values) => {
         Keyboard.dismiss();
         setDisable(true);
-        const response = await updatePassword({ ...info, idUser: user.idUser });
+        const response = await updatePassword({ ...values, idUser: user.idUser });
         setDisable(false);
 
         if (response.success) {
@@ -55,73 +60,82 @@ const ChangePassword = (props) => {
                             <BackButton />
                             <Title name={"Change password"} size={20} />
                         </View>
-                        <View>
-                            <View>
-                                <Text style={{ color: white, fontFamily: "SVN-Gotham-Light", paddingVertical: 5 }}>Current password</Text>
-                                <TextInput
-                                    placeholder="Enter your current password"
-                                    value={info.curPassword}
-                                    style={{ backgroundColor: gray5 }}
-                                    textColor={white}
-                                    onChangeText={(text) => handleFieldChange("curPassword", text)}
-                                    secureTextEntry={show}
-                                    right={
-                                        <TextInput.Icon
-                                            color={white}
-                                            icon={show ? "eye" : "eye-off"}
-                                            onPress={() => {
-                                                setShow((prev) => !prev);
-                                                Keyboard.dismiss();
-                                            }}
+                        <Formik initialValues={{ curPassword: "", newPassword: "", rePassword: "" }} validationSchema={validationSchema} onSubmit={(values) => onSubmitPress(values)}>
+                            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                                <View>
+                                    <View>
+                                        <Text style={{ color: white, fontFamily: "SVN-Gotham-Light", paddingVertical: 5 }}>Current password</Text>
+                                        <TextInput
+                                            placeholder="Enter your current password"
+                                            value={values.curPassword}
+                                            style={{ backgroundColor: gray5 }}
+                                            textColor={white}
+                                            onChangeText={handleChange("curPassword")}
+                                            onBlur={handleBlur("curPassword")}
+                                            secureTextEntry={show}
+                                            right={
+                                                <TextInput.Icon
+                                                    color={white}
+                                                    icon={show ? "eye" : "eye-off"}
+                                                    onPress={() => {
+                                                        setShow((prev) => !prev);
+                                                        Keyboard.dismiss();
+                                                    }}
+                                                />
+                                            }
                                         />
-                                    }
-                                />
-                            </View>
-                            <View>
-                                <Text style={{ color: white, fontFamily: "SVN-Gotham-Light", paddingVertical: 5 }}>New password</Text>
-                                <TextInput
-                                    placeholder="Enter new password"
-                                    value={info.newPassword}
-                                    style={{ backgroundColor: gray5 }}
-                                    textColor={white}
-                                    onChangeText={(text) => handleFieldChange("newPassword", text)}
-                                    secureTextEntry={show}
-                                    right={
-                                        <TextInput.Icon
-                                            color={white}
-                                            icon={show ? "eye" : "eye-off"}
-                                            onPress={() => {
-                                                setShow((prev) => !prev);
-                                                Keyboard.dismiss();
-                                            }}
+                                        {errors.curPassword && touched.curPassword && <Text style={{ color: "red" }}>{errors.curPassword}</Text>}
+                                    </View>
+                                    <View>
+                                        <Text style={{ color: white, fontFamily: "SVN-Gotham-Light", paddingVertical: 5 }}>New password</Text>
+                                        <TextInput
+                                            placeholder="Enter new password"
+                                            value={values.newPassword}
+                                            style={{ backgroundColor: gray5 }}
+                                            textColor={white}
+                                            onChangeText={handleChange("newPassword")}
+                                            onBlur={handleBlur("newPassword")}
+                                            secureTextEntry={show}
+                                            right={
+                                                <TextInput.Icon
+                                                    color={white}
+                                                    icon={show ? "eye" : "eye-off"}
+                                                    onPress={() => {
+                                                        setShow((prev) => !prev);
+                                                        Keyboard.dismiss();
+                                                    }}
+                                                />
+                                            }
                                         />
-                                    }
-                                />
-                            </View>
-                            <View>
-                                <Text style={{ color: white, fontFamily: "SVN-Gotham-Light", paddingVertical: 5 }}>Re-password</Text>
-                                <TextInput
-                                    placeholder="Enter new password"
-                                    value={info.rePassword}
-                                    style={{ backgroundColor: gray5 }}
-                                    textColor={white}
-                                    onChangeText={(text) => handleFieldChange("rePassword", text)}
-                                    secureTextEntry={show}
-                                    right={
-                                        <TextInput.Icon
-                                            color={white}
-                                            icon={show ? "eye" : "eye-off"}
-                                            onPress={() => {
-                                                setShow((prev) => !prev);
-                                                Keyboard.dismiss();
-                                            }}
+                                        {errors.newPassword && touched.newPassword && <Text style={{ color: "red" }}>{errors.newPassword}</Text>}
+                                    </View>
+                                    <View>
+                                        <Text style={{ color: white, fontFamily: "SVN-Gotham-Light", paddingVertical: 5 }}>Re-password</Text>
+                                        <TextInput
+                                            placeholder="Enter new password"
+                                            value={values.rePassword}
+                                            style={{ backgroundColor: gray5 }}
+                                            textColor={white}
+                                            onChangeText={handleChange("rePassword")}
+                                            secureTextEntry={show}
+                                            right={
+                                                <TextInput.Icon
+                                                    color={white}
+                                                    icon={show ? "eye" : "eye-off"}
+                                                    onPress={() => {
+                                                        setShow((prev) => !prev);
+                                                        Keyboard.dismiss();
+                                                    }}
+                                                />
+                                            }
                                         />
-                                    }
-                                />
-                            </View>
+                                        {errors.rePassword && touched.rePassword && <Text style={{ color: "red" }}>{errors.rePassword}</Text>}
+                                    </View>
 
-                            <Button children={"Submit"} onPress={onSubmitPress} loading={disable} />
-                        </View>
+                                    <Button children={"Submit"} onPress={handleSubmit} loading={disable} />
+                                </View>
+                            )}
+                        </Formik>
                     </View>
                 </SafeAreaView>
             </TouchableWithoutFeedback>
