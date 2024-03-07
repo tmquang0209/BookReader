0a7b51958256f19fecbb2d25fdbc4f6ed7a02d39
Trần Minh Quang <tmquang0209@gmail.com>
2024-03-07 19:48:17 +0700

    Fix formatting issues in API files

--- src/API/book.js
+++ src/API/book.js
@@ -6,7 +6,6 @@ import { filterAndMapBooks } from "../utils/utilsFilterMapBook";
 
 export const getInterestBook = async (userId) => {
     try {
-
         const response = await axios.get(`${API_URI}/api/getUserInterestCategory/${userId}`);
         const responseData = response.data;
         return responseData.success ? responseData : [];
@@ -17,7 +16,6 @@ export const getInterestBook = async (userId) => {
 };
 
 export const search = async (keyword, topic = "all") => {
-
     /* try {
         const response = await axios.post(`${API_URI}/api/search`, {
             bookName: keyword,
